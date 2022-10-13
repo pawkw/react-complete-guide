50473cd1d02eda757dabf1363c56cd061faa1fbe
pawkw <pawkw@user.noreply.github.com>
2022-10-13 01:08:19 -0400

    Lecture 36.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,31 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-    return (
+    const items = [
+        {
+            "date": new Date(2022, 2, 21),
+            "title": "Gift for Dad",
+            "amount": 20.0
+        },
+        {
+            "date": new Date(2022, 3, 20),
+            "title": "Gift for Mom",
+            "amount": 23
+        }
+    ];
+
+    const output = items.map((item) =>
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{item.date.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{item.title}</h2>
+                <div className='expense-item__price'>${item.amount.toString()}</div>
             </div>
         </div>
+    );
+
+    return (
+        <div>{output}</div>
     )
 }
 
