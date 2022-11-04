b8f0b20660fe2ab37291611412c6fedce504992a
pawkw <pawkw@users.noreply.github.com>
2022-11-03 21:28:40 -0400

    Lecture 61 and quiz 2.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,28 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
-    return <Card className='expenses'>
-            {props.expenses.map((item) => 
-                <ExpenseItem id={item.id}
-                title={item.title}
-                date={item.date}
-                amount={item.amount} />)}
-        </Card>
+        const [filterYear, setFilterYear] = useState('2020');
+
+        const onChangeYearHandler = enteredYear => {
+                setFilterYear(enteredYear);
+                console.log("Expenses changed filter year.");
+                console.log(filterYear);
+        };
+
+        return <div>
+                <Card className='expenses'>
+                        <ExpensesFilter selected={filterYear} onChangeYear={onChangeYearHandler} />
+                        {props.expenses.map((item) =>
+                                <ExpenseItem id={item.id}
+                                        title={item.title}
+                                        date={item.date}
+                                        amount={item.amount} />)}
+                </Card>
+        </div>
 }
 
 export default Expenses;
