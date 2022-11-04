18e0d53f0c6b110acdc02fb94e89e9ccd4e7d918
pawkw <pawkw@users.noreply.github.com>
2022-11-03 21:56:39 -0400

    Lecture 65.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    date: new Date(2022, 2, 21),
+    title: "Gift for Dad",
+    amount: 19.0
+  },
+  {
+    id: 'e2',
+    date: new Date(2022, 3, 20),
+    title: "Gift for Mom",
+    amount: 23.0
+  },
+  {
+    id: 'e3',
+    date: new Date(2022, 4, 19),
+    title: "Gift for Dave",
+    amount: 29.0
+  },
+  {
+    id: 'e4',
+    date: new Date(2022, 5, 18),
+    title: "Gift for Barb",
+    amount: 31.0
+  },
+]
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      date: new Date(2022, 2, 21),
-      title: "Gift for Dad",
-      amount: 19.0
-    },
-    {
-      id: 'e2',
-      date: new Date(2022, 3, 20),
-      title: "Gift for Mom",
-      amount: 23.0
-    },
-    {
-      id: 'e3',
-      date: new Date(2022, 4, 19),
-      title: "Gift for Dave",
-      amount: 29.0
-    },
-    {
-      id: 'e4',
-      date: new Date(2022, 5, 18),
-      title: "Gift for Barb",
-      amount: 31.0
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
     console.log("In app.js");
     console.log(expense);
+    setExpenses((previousState) => {
+      return [expense, ...previousState]
+    });
   };
-  
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
